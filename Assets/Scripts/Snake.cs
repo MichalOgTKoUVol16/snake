c922f449f4adb6dc4f1b2173286ca7033867e0bc
Michal Oglozinski <michal.oglozinski@picadilla.com>
2017-01-30 16:34:46 +0100

    init

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    public float speed = 1;
+
+    private Vector3 nextTurn;
+
+    public void Start()
+    {
+    }
+
+    public void TurnInput(float zRot)
+    {
+        if (!CanPickDirection(zRot))
+            return;
+
+        nextTurn = new Vector3(0, 0, zRot);
+    }
+
+    private bool CanPickDirection(float zRot)
+    {
+        return Mathf.Approximately(Mathf.Abs(Mathf.DeltaAngle(transform.rotation.eulerAngles.z, zRot)), 90f);
+    }
+
+    private bool CanTurnNow()
+    {
+        return transform.position.y % 0.64f < 0.1f;
+    }
+
+    public void Update()
+    {
+        if (nextTurn != Vector3.down && CanTurnNow())
+            Turn();
+
+        KeepMoving();
+    }
+
+    private void Turn()
+    {
+        transform.rotation = Quaternion.Euler(nextTurn);
+        nextTurn = Vector3.down;
+    }
+
+    private void KeepMoving()
+    {
+        transform.Translate(Vector2.up * speed * 0.1f * Time.deltaTime);
+    }
+}
+
---
+++ Assets/Scripts/SwipeScript.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+
+public class SwipeScript : MonoBehaviour
+{
+    Vector2 firstPressPos;
+    Vector2 secondPressPos;
+    Vector2 currentSwipe;
+    private Snake snake;
+
+    private void Start()
+    {
+        snake = GetComponent<Snake>();
+    }
+
+    public void Update()
+    {
+#if UNITY_EDITOR
+        SwipeEditor();
+#else
+         SwipeAndroid();
+#endif
+    }
+
+    public void SwipeAndroid()
+    {
+        if (Input.touches.Length > 0)
+        {
+            Touch t = Input.GetTouch(0);
+            if (t.phase == TouchPhase.Began)
+            {
+                //save began touch 2d point
+                firstPressPos = new Vector2(t.position.x, t.position.y);
+            }
+            if (t.phase == TouchPhase.Ended)
+            {
+                //save ended touch 2d point
+                secondPressPos = new Vector2(t.position.x, t.position.y);
+
+                //create vector from the two points
+                currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+                //normalize the 2d vector
+                currentSwipe.Normalize();
+
+                //swipe upwards
+                if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+                {
+                    Debug.Log("up swipe");
+                    snake.TurnInput(0f);
+                }
+                //swipe down
+                if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+                {
+                    Debug.Log("down swipe");
+                    snake.TurnInput(180f);
+                }
+                //swipe left
+                if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+                {
+                    Debug.Log("left swipe");
+                    snake.TurnInput(90f);
+                }
+                //swipe right
+                if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+                {
+                    Debug.Log("right swipe");
+                    snake.TurnInput(-90f);
+                }
+            }
+        }
+    }
+
+    public void SwipeEditor()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //save began touch 2d point
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            //save ended touch 2d point
+            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+            //create vector from the two points
+            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+            //normalize the 2d vector
+            currentSwipe.Normalize();
+
+            //swipe upwards
+            if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+            {
+                Debug.Log("up swipe");
+                snake.TurnInput(0f);
+            }
+            //swipe down
+            if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+            {
+                Debug.Log("down swipe");
+                snake.TurnInput(180f);
+            }
+            //swipe left
+            if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+            {
+                Debug.Log("left swipe");
+                snake.TurnInput(90f);
+            }
+            //swipe right
+            if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+            {
+                Debug.Log("right swipe");
+                snake.TurnInput(-90f);
+            }
+        }
+    }
+}
